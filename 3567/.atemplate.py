afc30d6b433d9ab51dba45c57088ad11fd8adab9
mysto <mystoganx2001@gmail.com>
2026-03-25 23:44:51 +1100

    SOLVED: 3546

--- 3567/.atemplate.py
+++ 3567/.atemplate.py
@@ -1,18 +1,23 @@
-class Solution(object):
+class Solution:
+    def reverseSubmatrix(self, grid, x, y, k):
 
-    def climbStairs(self, n):
-        sum = 1
-        before = 1
+        pointer = x + k -1 
 
-        for i in range(n-1):
-            after = sum
-            sum += before
-            before = after
 
-        return sum
+        while pointer > x:
+
+            for i in range(k):
+                grid[x][y + i], grid[pointer][y + i] = grid[pointer][y + i], grid[x][y + i]
+            
+            x += 1
+            pointer -= 1
+                
+
+        return grid
 
 if __name__ == "__main__":
     yes = Solution()
 
-    print(yes.climbStairs(3))
+    print(yes.reverseSubmatrix([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]], 1, 0, 3) == [[1,2,3,4],[13,14,15,8],[9,10,11,12],[5,6,7,16]] )
+    print(yes.reverseSubmatrix([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]], 1, 0, 3))
 
